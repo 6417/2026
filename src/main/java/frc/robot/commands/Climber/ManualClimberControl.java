db35b287405072c3b7e6140a1b64a0df95471bdb
Florin42 <florin.hegner@bwz-rappi.ch>
2026-02-20 22:24:29 +0100

    Prepare to test the Swerve Drive. Commented intake, turret and climber OUT.

--- src/main/java/frc/robot/commands/Climber/ManualClimberControl.java
+++ src/main/java/frc/robot/commands/Climber/ManualClimberControl.java
@@ -11,19 +11,19 @@ public class ManualClimberControl extends Command {
     public ManualClimberControl(DoubleSupplier percentSupplier) {
         // Supplier allows live joystick input without storing controller here.
         this.percentSupplier = percentSupplier;
-        addRequirements(RobotContainer.climber);
+        // addRequirements(RobotContainer.climber);
     }
 
     @Override
     public void execute() {
         // Pass through manual input each cycle.
-        RobotContainer.climber.setManualPercent(percentSupplier.getAsDouble());
+        // RobotContainer.climber.setManualPercent(percentSupplier.getAsDouble());
     }
 
     @Override
     public void end(boolean interrupted) {
         // Stop motor on release or interruption.
-        RobotContainer.climber.stop();
+        // RobotContainer.climber.stop();
     }
 
     @Override
