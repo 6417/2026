8767e804a7e52b51cac924f0e6e16d731faf8e40
Florin42 <florin.hegner@bwz-rappi.ch>
2026-03-01 11:28:10 +0100

    Fix Warning with Intake Motor created as SpakMax instead of SparkFlex

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,21 +5,22 @@ import com.revrobotics.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.fridowpi.motors.FridoSparkFlex;
 import frc.fridowpi.motors.FridoSparkMax;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private final FridoSparkMax intakeMotor;
+    private final FridoSparkFlex intakeMotor;
 
     public IntakeSubsystem() {
-        intakeMotor = new FridoSparkMax(Constants.Intake.intakeMotorId);
+        intakeMotor = new FridoSparkFlex(Constants.Intake.intakeMotorId);
 
         intakeMotor.setIdleMode(Constants.Intake.idleMode);
         intakeMotor.setInverted(Constants.Intake.intakeMotorInverted);
 
         SparkMaxConfig config = new SparkMaxConfig();
         config.openLoopRampRate(Constants.Intake.openLoopRampRate);
-        intakeMotor.asSparkMax().configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+        intakeMotor.asSparkFlex().configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
 
         // Could be added back for auto-intake
         // setDefaultCommand(new IntakeCommand(this));
